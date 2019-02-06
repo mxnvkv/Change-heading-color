d17e0df021fd3633b59dd4b6a46314a0d0e92320
mxnvkv <38319843+mxnvkv@users.noreply.github.com>
2019-02-06 17:04:31 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('.form');
+const submitText = document.querySelector('input[type=text]');
+const submit = document.querySelector('input[type=submit]');
+const output = document.querySelector('.output');
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const li = document.createElement('li');
+    li.textContent = submitText.value;
+    output.appendChild(li);
+    submitText.value = '';
+});
